0e1a2bc471f11f8eefba8469b47422458ca6cf05
phucngdev <98637507+phucngdev@users.noreply.github.com>
2025-02-24 18:44:02 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://frontend-teelab-vercel.vercel.app",
+      "https://task-manager-fawn-seven-48.vercel.app",
     ],
     credentials: true,
   })
